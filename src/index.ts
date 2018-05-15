3f1a739657049a6d6451d8f52a9220bd36c6fbe3
mkery <mkery@cs.cmu.edu>
2018-05-15 17:03:48 -0400

    still experimenting trying to get parsing

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,11 @@ import {
 } from '@jupyterlab/application';
 
 import {
-  NotebookPanel
+  KernelMessage, Kernel
+} from '@jupyterlab/services';
+
+import {
+  NotebookPanel, Notebook
 } from '@jupyterlab/notebook';
 
 import {
@@ -16,6 +20,7 @@ import {
 
 import '../style/index.css';
 
+//import * as fs from 'fs-extra';
 
 /**
  * Initialization data for the Verdant extension.
@@ -28,6 +33,10 @@ const extension: JupyterLabPlugin<void> = {
     const panel = new StackedPanel();
     const tabs = new TabBar<Widget>({ orientation: 'vertical' });
     const header = document.createElement('header');
+    var activePanel: NotebookPanel;
+    var activeKernel: Kernel.IKernelConnection;
+    var notebook : Notebook; //the currently active notebook Verdant is working on
+    //const parserText : string = fs.readFileSync('py2ast.py', 'utf-8')
     //const contentPanel = new StackedPanel();
 
     restorer.add(panel, 'verdant-manager');
@@ -49,6 +58,16 @@ const extension: JupyterLabPlugin<void> = {
     app.restored.then(() => {
       const populate = () => {
         tabs.clearTabs();
+        var widg = shell.currentWidget
+        if(widg instanceof NotebookPanel)
+          if(!activePanel || activePanel !== widg)
+          {
+            activePanel = widg
+            activePanel.ready.then(() => {
+              notebook = activePanel.notebook
+              openHistory()
+            })
+          }
         each(shell.widgets('main'), widget => {
           if(widget instanceof NotebookPanel)
             tabs.addTab(widget.title);
@@ -58,8 +77,61 @@ const extension: JupyterLabPlugin<void> = {
       // Connect signal handlers.
       shell.layoutModified.connect(() => { populate(); });
 
+      tabs.tabActivateRequested.connect((sender, tab) => {
+        shell.activateById(tab.title.owner.id);
+      });
+      tabs.tabCloseRequested.connect((sender, tab) => {
+        tab.title.owner.close();
+      });
+
       // Populate the tab manager.
       populate();
+
+      const getKernel = (): Promise<Kernel.IKernelConnection> => {
+        return new Promise((accept, reject) => {
+          if(!activeKernel && activePanel.context.session.kernel)
+             accept(activePanel.context.session.kernel)
+          else
+          {
+              Kernel.startNew().then(kernel => (accept(kernel)))
+          }
+        })
+      }
+
+      //testing get code from Notebook
+      const openHistory = () => {
+        console.log("active notebook is", notebook)
+        getKernel().then((kernel: Kernel.IKernelConnection) => {
+          activeKernel = kernel
+          notebook.widgets.forEach( (item, index) => {
+            console.log("found cell", item)
+            var text : string = item.editor.model.value.text
+            console.log("text is ", text)
+            codeToAst(text, kernel)
+          })
+        })
+      }
+
+      const codeToAst = (code: string, kernel: Kernel.IKernelConnection): Promise<KernelMessage.IExecuteReplyMsg> => {
+        //console.log("we're going to run ", parserText)
+        var command = "%run ./py2ast "+code
+
+        // Override the default for `stop_on_error`.
+        let content: KernelMessage.IExecuteRequest = {
+          code: command,
+          stop_on_error: true
+        };
+        let future = kernel.requestExecute(content, false);
+        future.onReply = (msg: KernelMessage.IExecuteReplyMsg): void => {
+          console.log(": ", msg.content)
+        }
+        future.onIOPub = (msg: KernelMessage.IIOPubMessage): void => {
+          console.log(": ", msg.content)
+        }
+        return future.done as Promise<KernelMessage.IExecuteReplyMsg>;
+      }
+
+
     });
 
   },
