d1288c6f82805c5b6b856822a8e2d436f5375175
mkery <mkery@cs.cmu.edu>
2018-12-05 13:52:43 -0500

    search accordian boxes

--- src/panel/search.ts
+++ src/panel/search.ts
@@ -6,12 +6,14 @@ import { Nodey } from "../model/nodey";
 const PANEL = "v-VerdantPanel-content";
 const SEARCH_CONTAINER = "v-VerdantPanel-searchContainer";
 const SEARCH_ICON = "v-VerdantPanel-searchIcon";
+const SEARCH_CONTENT = "v-VerdantPanel-searchContent";
 //const FILTER_OPTS_ICON = "v-VerdantPanel-filterOptsIcon";
 const SEARCH_TEXT = "v-VerdantPanel-searchText";
 const RESULT_CATEGORY = "VerdantPanel-search-results-category";
 const RESULT_HEADER = "VerdantPanel-search-results-header";
 const RESULT_CATEGORY_CONTENT = "VerdantPanel-search-results-category-content";
 const RESULT_CATEGORY_FOOTER = "VerdantPanel-search-results-category-footer";
+const RESULT_HEADER_BUTTON = "VerdantPanel-search-results-header-button";
 
 export class Search extends Widget {
   readonly history: History;
@@ -46,6 +48,7 @@ export class Search extends Widget {
     this.node.appendChild(searchContainer);
 
     this.searchContent = document.createElement("div");
+    this.searchContent.classList.add(SEARCH_CONTENT);
     this.node.appendChild(this.searchContent);
   }
 
@@ -71,12 +74,11 @@ export class Search extends Widget {
     area.classList.add(RESULT_CATEGORY);
     let label = document.createElement("div");
     label.classList.add(RESULT_HEADER);
-    label.textContent = "(" + results.length + " found) " + header;
     area.appendChild(label);
+
     let content = document.createElement("div");
     content.classList.add(RESULT_CATEGORY_CONTENT);
     area.appendChild(content);
-
     results.forEach(item => {
       let elem = VersionSampler.sample(this.history, item);
       content.appendChild(elem);
@@ -84,8 +86,48 @@ export class Search extends Widget {
 
     let footer = document.createElement("div");
     footer.classList.add(RESULT_CATEGORY_FOOTER);
-    footer.textContent = "...";
     area.appendChild(footer);
+
+    /*
+    * animated button, thus the extra divs
+    */
+    let button = document.createElement("div");
+    button.classList.add(RESULT_HEADER_BUTTON);
+    button.classList.add("closed");
+    label.appendChild(button);
+    let circle = document.createElement("div");
+    circle.classList.add("circle");
+    button.appendChild(circle);
+    let horizontal = document.createElement("div");
+    horizontal.classList.add("horizontal");
+    circle.appendChild(horizontal);
+    let vertical = document.createElement("div");
+    vertical.classList.add("vertical");
+    circle.appendChild(vertical);
+    button.addEventListener("mousedown", () => {
+      if (button.classList.contains("opened")) {
+        button.classList.remove("opened");
+        button.classList.add("closed");
+        setTimeout(() => {
+          content.style.display = "";
+          footer.style.display = "";
+        }, 300);
+      } else if (button.classList.contains("closed")) {
+        button.classList.remove("closed");
+        button.classList.add("opened");
+        setTimeout(() => {
+          content.style.display = "block";
+          footer.style.display = "block";
+        }, 300);
+      }
+    });
+
+    let textContent = document.createElement("span");
+    if (results.length > 1)
+      textContent.textContent = "(" + results.length + " matches) " + header;
+    else textContent.textContent = "(" + results.length + " match) " + header;
+    label.appendChild(textContent);
+
     return area;
   }
 }
